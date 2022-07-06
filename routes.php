944c6f70aa4acc8e9b03df3742ed291113d29cee
Marvin Zimmermann <marvin_zimmermann1@sluz.ch>
2022-07-06 13:09:07 +0200

    Revert "Framework added"
    This reverts commit 078dcbf8b6ea46bbca964b4b1685e389aa52c1b5.

--- routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$router = new Router();
-
-$router->define([
-    '' => 'app/Controllers/GuestbookController.php',
-    'list' => 'app/Controllers/GuestbookController.php',
-    'add' => 'app/Controllers/GuestbookCreateController.php'
-]);
